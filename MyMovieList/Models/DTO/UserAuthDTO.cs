b78b66df4bbd808104b510e46fd0ab41b5a62c78
HenriqueSabino <h.sabinocosta@gmail.com>
2023-03-14 17:39:30 -0300

    Revert "Revert "Adding SQLLite as a database option""
    This reverts commit f348e2d9d23e35a5a17ea52e901a3d20c5a32860.

---
+++ MyMovieList/Models/DTO/UserAuthDTO.cs
@@ -0,0 +1,10 @@
+namespace MyMovieList.Models.DTO;
+
+public class UserAuthDTO
+{
+    public string? Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Email { get; set; }
+}
